1f5d05c8c48f7cfcd489e4ab5e29c8edce5d68d5
kishalay01 <89468475+kishalay01@users.noreply.github.com>
2022-02-16 13:32:02 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,19 @@
+
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="index.php" style="color :  #1BAF9A;"><b>SPARK BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #38ddfa;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color :#38ddfa;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #38ddfa;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
